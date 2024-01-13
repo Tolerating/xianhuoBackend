caf7aa8ff3b0e7487fa53241add9631dd71df707
lk <354378485@qq.com>
2024-01-13 19:09:29 +0800

    test: dao测试

--- src/test/java/com/xianhuo/xianhuobackend/XianHuoBackendApplicationTests.java
+++ src/test/java/com/xianhuo/xianhuobackend/XianHuoBackendApplicationTests.java
@@ -1,13 +1,17 @@
 package com.xianhuo.xianhuobackend;
 
+import com.xianhuo.xianhuobackend.service.DispatchModeService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class XianHuoBackendApplicationTests {
-
+    @Autowired
+    private DispatchModeService dispatchModeService;
     @Test
     void contextLoads() {
+        dispatchModeService.getUsableDispatchModeBySellId(1l);
     }
 
 }
