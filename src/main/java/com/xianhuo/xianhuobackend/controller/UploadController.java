38aba7f31ef45e6bce4d430c9010de864a712bc1
lk <354378485@qq.com>
2024-01-19 17:04:03 +0800

    feat: 上传图片

--- src/main/java/com/xianhuo/xianhuobackend/controller/UploadController.java
+++ src/main/java/com/xianhuo/xianhuobackend/controller/UploadController.java
@@ -3,12 +3,14 @@ package com.xianhuo.xianhuobackend.controller;
 import com.xianhuo.xianhuobackend.common.ResponseResult;
 import com.xianhuo.xianhuobackend.utils.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
 
 @RestController
 @CrossOrigin
@@ -16,29 +18,31 @@ import java.io.IOException;
 public class UploadController {
     @Autowired
     private HttpServletRequest httpServletRequest;
+    @Value("${file-dir}")
+    String fileDir;
 
     //上传单个文件
     @PostMapping("/upload")
     public ResponseResult uploadFile(@RequestPart("file") MultipartFile file) throws IOException {
         String authorization = httpServletRequest.getHeader("authorization");
         String id = JWTUtil.parseJWT(authorization).getId();
-        File mainF = new File(System.getProperty("user.dir")+"\\src\\main\\resources\\static\\productImg\\"+id);
+        String url = "/img/" + id;
+        File mainF = new File(fileDir + url);
         if (!mainF.exists()) {
             mainF.mkdirs();
         }
 
-        if(!file.isEmpty()){
-            System.out.println(file.getOriginalFilename());
-            String path = System.getProperty("user.dir")+"\\src\\main\\resources\\static\\productImg\\"+id+"\\"+file.getOriginalFilename();
-            if (!new File(path).exists()) {
-                file.transferTo(new File(path));
-            }
-            String url = httpServletRequest.getScheme() + "://"+ httpServletRequest.getServerName() +
-                    ":" + httpServletRequest.getServerPort() + httpServletRequest.getContextPath() + "/img/"+id+"/"+file.getOriginalFilename();
-            return ResponseResult.ok(url,"success");
+        if (!file.isEmpty()) {
+
+            String path = String.format("%s/%s/%s", fileDir, url, file.getOriginalFilename());
+//            if (!new File(path).exists()) {
+//            }
+            file.transferTo(new File(path));
+
+            return ResponseResult.ok(url+"/"+file.getOriginalFilename(), "success");
 
         }
 
-        return ResponseResult.ok("图片不能为空","error");
+        return ResponseResult.ok("图片不能为空", "error");
     }
 }
