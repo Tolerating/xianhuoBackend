e81d589c35fb5815ea88e9a37fa02664ccd234c0
lk <354378485@qq.com>
2024-01-27 22:26:52 +0800

    style: 更改用户表部分字段允许为空

--- data.sql
+++ data.sql
@@ -1,12 +1,12 @@
 create table if not exists users
 (
     id            bigint primary key auto_increment,
-    name          varchar(100) not null comment '用户昵称',
-    phone         varchar(50)  not null comment '电话',
+    name          varchar(100) comment '用户昵称',
+    phone         varchar(50)  comment '电话',
     password      varchar(50)  not null comment '密码',
-    school     varchar(50)     not null comment '学校名字',
+    school     varchar(50)     comment '学校名字',
     avatar        text      default 'default.jpg' comment '用户头像',
-    email         varchar(50) comment '邮箱地址',
+    email         varchar(50) not null comment '邮箱地址',
     identity_card varchar(50) comment '身份证',
     stu_number    varchar(50) comment '学生学号',
     grade         varchar(50) comment '班级',
