244de0b2507e2b4f496771da7fe9aab5d1d438e2
lk <354378485@qq.com>
2024-02-02 22:12:18 +0800

    style: 添加获取已经发布商品和数量接口

--- src/main/java/com/xianhuo/xianhuobackend/controller/ProductController.java
+++ src/main/java/com/xianhuo/xianhuobackend/controller/ProductController.java
@@ -1,13 +1,17 @@
 package com.xianhuo.xianhuobackend.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.xianhuo.xianhuobackend.common.ResponseProcess;
 import com.xianhuo.xianhuobackend.common.ResponseResult;
 import com.xianhuo.xianhuobackend.entity.Product;
 import com.xianhuo.xianhuobackend.service.ProductService;
+import com.xianhuo.xianhuobackend.utils.JWTUtil;
 import jdk.nashorn.internal.objects.annotations.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.support.HttpRequestHandlerServlet;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
@@ -16,11 +20,14 @@ import java.util.List;
 public class ProductController {
     @Autowired
     private ProductService productService;
+    @Autowired
+    private HttpServletRequest httpServletRequest;
 
     @GetMapping("/product")
     public List<Product> allProduct(){
         return productService.list();
     }
+//    根据商品ID获取商品全部信息
     @GetMapping("/product/{productId}")
     public ResponseResult<Product> productById(@PathVariable("productId") Long productId){
         Product product = productService.getById(productId);
@@ -34,9 +41,27 @@ public class ProductController {
         return null;
     }
 
+//    发布商品
     @PostMapping("/product")
     public ResponseResult increaseProduct(@RequestBody Product product){
         boolean saved = productService.save(product);
         return ResponseProcess.returnString(saved,"发布成功","发布失败");
+    }
+// 获取已经发布的商品
+    @GetMapping("/product/released")
+    public ResponseResult productReleased(){
+        String authorization = httpServletRequest.getHeader("authorization");
+        String id = JWTUtil.parseJWT(authorization).getId();
+        List<Product> list = productService.list(new LambdaQueryWrapper<Product>().eq(Product::getUserId, id));
+        return ResponseProcess.returnList(list);
+    }
+
+//    获取发布商品数量
+    @GetMapping("/product/count")
+    public ResponseResult productCount(){
+        String authorization = httpServletRequest.getHeader("authorization");
+        String id = JWTUtil.parseJWT(authorization).getId();
+        long count = productService.count(new LambdaQueryWrapper<Product>().eq(Product::getUserId, id));
+        return ResponseProcess.returnLong(count,"success","fail");
     }
 }
