dfb2c627922bdbe563700528eea3fcd2f17560eb
lk <354378485@qq.com>
2024-02-19 22:27:44 +0800

    style: websocket单聊

---
+++ src/main/java/com/xianhuo/xianhuobackend/config/WebSocketConfig.java
@@ -0,0 +1,35 @@
+package com.xianhuo.xianhuobackend.config;
+
+import com.xianhuo.xianhuobackend.controller.MyWebSocket;
+import com.xianhuo.xianhuobackend.service.ChatListService;
+import com.xianhuo.xianhuobackend.service.ChatMessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+import javax.servlet.http.HttpSession;
+import javax.websocket.HandshakeResponse;
+import javax.websocket.server.HandshakeRequest;
+import javax.websocket.server.ServerEndpointConfig;
+
+@Configuration
+public class WebSocketConfig extends ServerEndpointConfig.Configurator{
+
+    @Override
+    public void modifyHandshake(ServerEndpointConfig sec, HandshakeRequest request, HandshakeResponse response) {
+        HttpSession session = (HttpSession) request.getHttpSession();
+        sec.getUserProperties().put(HttpSession.class.getName(), session);
+    }
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+
+    @Autowired
+    public void setSenderService(ChatListService chatListService, ChatMessageService chatMessageService){
+        MyWebSocket.chatListService = chatListService;
+        MyWebSocket.chatMessageService = chatMessageService;
+    }
+
+}
